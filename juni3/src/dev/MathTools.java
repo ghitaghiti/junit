dafc44066c5e988e18bf48a116d45cff2cf5113a
ghita ghiti <ghitaghiti@gmail.com>
2024-01-30 13:56:36 +0100

    create packages and classes with methods

---
+++ juni3/src/dev/MathTools.java
@@ -0,0 +1,16 @@
+package dev;
+
+public class MathTools {
+	public  double carre(double number) {
+		return Math.pow(number,2);
+		
+	}
+
+//	public static void main(String[] args) {
+//		
+//		double r=carre(2);
+//		System.out.println(r);
+//
+//	}
+
+}
---
+++ juni3/src/test/MathToolsTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class MathToolsTest extends TestCase{
+	@Override
+	public void setUp() {
+		
+	}
+	@Override
+	public void tearDown() {
+		
+	}
+
+}
