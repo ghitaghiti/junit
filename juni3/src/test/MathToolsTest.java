133a63463a705785f4a64be751b7baa879e234f6
ghita ghiti <ghitaghiti@gmail.com>
2024-01-30 14:55:46 +0100

    end ex

--- juni3/src/test/MathToolsTest.java
+++ juni3/src/test/MathToolsTest.java
@@ -1,15 +1,31 @@
 package test;
 
+import dev.MathTools;
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import test.MathToolsTest;
 
 public class MathToolsTest extends TestCase{
-	@Override
-	public void setUp() {
-		
+	MathTools mathTools;
+	
+	public static Test suite(){
+		return new TestSuite(MathToolsTest.class);
 	}
-	@Override
+	
+	public void setUp() {
+		mathTools=new MathTools();
+	}
+	
+
 	public void tearDown() {
 		
+	}
+	
+	public void testCarre() {
+		double expected = mathTools.carre(5.0);
+		double local=25.0;
+		assertEquals("error",expected,local);
 	}
 
 }
